f6bf869e9e2dd482d5b2840a7cd2cf27dfae5efb
TsubasaNishihara <63089960+TsubasaNishihara@users.noreply.github.com>
2020-08-01 00:16:04 +0900

    Update

--- function.py
+++ function.py
@@ -2,7 +2,168 @@ import os
 import random
 import numpy as np
 from time import gmtime, strftime
+from tensorflow.keras.layers import Input, Dense, Activation, BatchNormalization
+from tensorflow.keras.models import Model
+from tensorflow.keras.losses import mean_squared_error, categorical_crossentropy
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.callbacks import TensorBoard, Callback
+
+
+# 浸透サブネットおよび全体のネットワークの構成
+def network(input_shape, num_percfeature, num_classes,
+            num_layers_percnet, num_layers_intnet,
+            num_elements_percnet, num_elements_intnet):
+    input_img = Input(shape=input_shape)
+    x = input_img
+    for i in range(num_layers_percnet):
+        if i == num_layers_percnet - 1:
+            x = Dense(num_percfeature)(x)
+        else:
+            x = Dense(num_elements_percnet)(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+    feature = x
+    percnet = Model(input_img, feature)
+
+    x = percnet.output
+    for i in range(num_layers_intnet - 1):
+        x = Dense(num_elements_intnet)(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+    x = Dense(num_classes)(x)
+    output = Activation('softmax')(x)
+    network = Model(percnet.input, output)
+
+    return percnet, network
+
+
+# MNISTデータセットの準備
+class MNISTDataset():
+    def __init__(self):
+        self.num_input = 784
+        self.num_classes = 10
+
+    # MNISTデータセットの取得
+    def get_data(self):
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        # 出力データをone-hotベクトルによる表現にする
+        y_train = to_categorical(y_train, self.num_classes)
+        y_test = to_categorical(y_test, self.num_classes)
+        # 各データが0~1の値となるように調整
+        x_train = x_train.astype('float32') / 255
+        x_test = x_test.astype('float32') / 255
+        # 28*28ピクセルのデータを784個のデータに平滑化
+        x_train = x_train.reshape([len(x_train), self.num_input])
+        x_test = x_test.reshape([len(x_test), self.num_input])
+        return x_train, y_train, x_test, y_test
+
+
+# 主データと補助データの準備
+def get_main_aux_data(x, y, validation_split, test_split, shuffle_rate):
+    x_main = shuffle_pixel(x, shuffle_rate)
+    x = np.concatenate([x, x_main], axis=1)
+    id_test = int(test_split * x.shape[0])
+    id_val = int(validation_split * x.shape[0])
+    x_train = x[:-(id_val + id_test)]
+    y_train = y[:-(id_val + id_test)]
+    x_val = x[-(id_val + id_test):-id_test]
+    y_val = y[-(id_val + id_test):-id_test]
+    x_test = x[-id_test:]
+    y_test = y[-id_test:]
+    return x_train, y_train, x_val, y_val, x_test, y_test
+
+
+# 学習させるためのクラス
+class Trainer():
+
+    def __init__(self, model_percnet, model_wholenet, optimizer, verbose):
+        self.model_percnet = model_percnet
+        self.model_wholenet = model_wholenet
+        self.model_percnet.compile(loss=mean_squared_error, optimizer=optimizer)
+        self.model_wholenet.compile(loss=categorical_crossentropy, optimizer=optimizer, metrics=['accuracy'])
+        self.verbose = verbose
+
+    # 学習
+    def train(self, x_train, y_train,
+              x_val, y_val,
+              auxdt_size,
+              batch_size,
+              epochs_prior, epochs_perc, epochs_adj,
+              decay,
+              history):
+        # 1：事前学習
+        self.model_wholenet.summary()
+        self.model_wholenet.fit(x_train, y_train,
+                                epochs=epochs_prior,
+                                batch_size=batch_size,
+                                verbose=self.verbose,
+                                validation_data=(x_val, y_val),
+                                callbacks=[history])
+
+        # 浸透特徴の保存
+        perc_feature = self.model_percnet.predict(x_train)
+        perc_feature_val = self.model_percnet.predict(x_val)
+
+        # 2：浸透学習
+        epoch = 0
+        loss = 1
+        non_perc_rate = 1  # 非浸透率の初期値
+        nprate_min = 1e-8  # 非浸透率の閾値
+        loss_min = 1e-5  # 損失関数の値の閾値
+        # 補助データに非浸透率を掛けるための配列を作成
+        non_perc_vec = np.ones(x_train.shape[1])
+        non_perc_vec[:auxdt_size] = 1 - decay
+
+        '''
+        # 統合サブネットの重みを固定
+        for i in range(3 * (layers_intnet - 1) + 2):
+            network.layers[-i-1].trainable = False
+        network.compile(optimizer=optimizer, loss=categorical_crossentropy, metrics=['accuracy'])
+        '''
+
+        # 浸透学習
+        self.model_percnet.summary()
+        while non_perc_rate > nprate_min or loss > loss_min:
+            non_perc_rate = (1 - decay) ** epoch
+            print('Non-Percolation Rate =', non_perc_rate)
+            self.model_percnet.fit(x_train, perc_feature,
+                                   initial_epoch=epochs_prior + epoch, epochs=epochs_prior + epoch + 1,
+                                   batch_size=batch_size,
+                                   verbose=self.verbose,
+                                   validation_data=(x_val, perc_feature_val))
+            ev = self.model_wholenet.evaluate(x_train, y_train, batch_size=batch_size, verbose=0)
+            ev_val = self.model_wholenet.evaluate(x_val, y_val, batch_size=batch_size, verbose=0)
+            history.losses.append(ev[0])
+            history.accuracy.append(ev[1])
+            history.losses_val.append(ev_val[0])
+            history.accuracy_val.append(ev_val[1])
+            loss = self.model_percnet.evaluate(x_train, perc_feature, verbose=0)
+            x_train *= non_perc_vec
+            epoch += 1
+            if epoch >= epochs_perc:
+                break
+
+        # 3：微調整
+        '''
+        # 統合サブネットの重み固定を解除
+        for i in range(3 * (layers_intnet - 1) + 2):
+            network.layers[-i-1].trainable = True
+        network.compile(optimizer=optimizer, loss=categorical_crossentropy, metrics=['accuracy'])
+        network.summary()
+        '''
+        if True:  # 微調整を行う条件を入れる(現状は常に微調整を行う)
+            non_perc_vec[:auxdt_size] = 0
+            x_train *= non_perc_vec
+            self.model_wholenet.fit(x_train, y_train,
+                                    initial_epoch=epochs_prior + epochs_perc,
+                                    epochs=epochs_prior + epoch + epochs_adj,
+                                    batch_size=batch_size,
+                                    verbose=self.verbose,
+                                    validation_data=(x_val, y_val),
+                                    callbacks=[history])
+
+        return history
 
 
 # Tensorboardの作成
@@ -58,9 +219,4 @@ def shuffle_datasets(x, y):
     x_out = x[id_shuffled, :]
     y_out = y[id_shuffled, :]
     return x_out, y_out
-
-
-def concat(dt1, dt2):
-    dt = np.concatenate([dt1, dt2], axis=1)
-    return dt
 
--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 # 浸透学習の実装
 
-from tensorflow.keras.layers import Input, Dense, Activation, BatchNormalization
-from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow.keras.losses import mean_squared_error, categorical_crossentropy
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.utils import to_categorical
 import numpy as np
 import matplotlib.pyplot as plt
-from function import make_tensorboard, shuffle_pixel, shuffle_datasets, LossAccHistory
+from function import network, MNISTDataset, get_main_aux_data, Trainer, make_tensorboard, shuffle_pixel, shuffle_datasets, LossAccHistory
 
 maindt_size = 784       # 主データのサイズ
 subdt_size = 784        # 補助データのサイズ
@@ -26,65 +22,30 @@ batch_size = 1024       # バッチサイズ
 validation_split = 1 / 7  # 評価に用いるデータの割合
 test_split = 1 / 7        # テストに用いるデータの割合
 verbose = 2             # 学習進捗の表示モード
-decay = 0.5            # 減衰率
+decay = 0.05            # 減衰率
 optimizer = Adam(lr=0.0001)      # 最適化アルゴリズム
 # callbacks = [make_tensorboard(set_dir_name='log')]  # コールバック
 
 
-# ニューラルネットワークの設計
-# 浸透サブネット
-input_img = Input(shape=(maindt_size+subdt_size,))
-x = input_img
-for i in range(layers_percnet):
-    if i == layers_percnet - 1:
-        x = Dense(percfeature_size)(x)
-    else:
-        x = Dense(percnet_size)(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-feature = x
+# ニューラルネットワークの構成
+percnet, network = network((maindt_size+subdt_size,),
+                           percfeature_size, output_size,
+                           layers_percnet, layers_intnet,
+                           percnet_size, intnet_size)
 
-# 浸透サブネットの設定
-percnet = Model(input_img, feature)
-
-# 全体のネットワークの設定
-x = percnet.output
-for i in range(layers_intnet - 1):
-    x = Dense(intnet_size)(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-x = Dense(output_size)(x)
-output = Activation('softmax')(x)
-
-network = Model(percnet.input, output)
 percnet.compile(optimizer=optimizer, loss=mean_squared_error)
 network.compile(optimizer=optimizer, loss=categorical_crossentropy, metrics=['accuracy'])
 
 
 # MNISTデータの読み込み
-(x_train_aux, y_train), (x_test_aux, y_test) = mnist.load_data()
-y_train = to_categorical(y_train, output_size)
-y_test = to_categorical(y_test, output_size)
-# 各データが0~1の値となるように調整
-x_train_aux = x_train_aux.astype('float32') / 255
-x_test_aux = x_test_aux.astype('float32') / 255
-# 28*28ピクセルのデータを784個のデータに平滑化
-x_train_aux = x_train_aux.reshape([len(x_train_aux), subdt_size])
-x_test_aux = x_test_aux.reshape([len(x_test_aux), subdt_size])
+datasets = MNISTDataset()
+x_train, y_train, x_test, y_test = datasets.get_data()
 # TrainデータとTestデータを結合して，それをTrain，Validation，Testデータに分ける．
-x_aux = np.concatenate([x_train_aux, x_test_aux], axis=0)
+x = np.concatenate([x_train, x_test], axis=0)
 y = np.concatenate([y_train, y_test], axis=0)
-# x_aux, y = shuffle_datasets(x_aux, y)
-x_main = shuffle_pixel(x_aux, shuffle_rate)
-x = np.concatenate([x_aux, x_main], axis=1)
-id_test = int(test_split * x.shape[0])
-id_val = int(validation_split * x.shape[0])
-x_train = x[:-(id_val+id_test)]
-y_train = y[:-(id_val+id_test)]
-x_val = x[-(id_val+id_test):-id_test]
-y_val = y[-(id_val+id_test):-id_test]
-x_test = x[-id_test:]
-y_test = y[-id_test:]
+# x, y = shuffle_datasets(x, y)
+x_train, y_train, x_val, y_val, x_test, y_test = get_main_aux_data(x, y, validation_split, test_split, shuffle_rate)
+# ValidationとTestデータの補助データは全て0にする
 x_val[:, :subdt_size] = 0
 x_test[:, :subdt_size] = 0
 
@@ -123,79 +84,18 @@ for i in range(n):
 plt.show()
 '''
 
-# 事前学習
-network.summary()
-history_list = LossAccHistory()
-network.fit(x_train, y_train,
-            epochs=epochs_prior,
-            batch_size=batch_size,
-            verbose=verbose,
-            validation_data=(x_val, y_val),
-            callbacks=[history_list])
-
-# 浸透特徴の保存
-perc_feature = percnet.predict(x_train)
-perc_feature_val = percnet.predict(x_val)
-
-# 浸透学習
-epoch = 0
-loss = 1
-non_perc_rate = 1   # 非浸透率
-nprate_min = 1e-8   # 非浸透率の閾値
-loss_min = 1e-5     # 損失関数の値の閾値
-# 補助データに非浸透率を掛けるための配列を作成
-non_perc_vec = np.ones(x_train.shape[1])
-non_perc_vec[:subdt_size] = 1 - decay
-
-'''
-# 統合サブネットの重みを固定
-for i in range(3 * (layers_intnet - 1) + 2):
-    network.layers[-i-1].trainable = False
-network.compile(optimizer=optimizer, loss=categorical_crossentropy, metrics=['accuracy'])
-'''
-
-network.summary()
 # 学習
-while non_perc_rate > nprate_min or loss > loss_min:
-    non_perc_rate = (1 - decay) ** epoch
-    print('Non Percolation Rate =', non_perc_rate)
-    percnet.fit(x_train, perc_feature,
-                initial_epoch=epochs_prior+epoch, epochs=epochs_prior+epoch+1,
-                batch_size=batch_size,
-                verbose=verbose,
-                validation_data=(x_val, perc_feature_val))
-    ev = network.evaluate(x_train, y_train, batch_size=batch_size, verbose=0)
-    ev_val = network.evaluate(x_val, y_val, batch_size=batch_size, verbose=0)
-    history_list.losses.append(ev[0])
-    history_list.accuracy.append(ev[1])
-    history_list.losses_val.append(ev_val[0])
-    history_list.accuracy_val.append(ev_val[1])
-    loss = percnet.evaluate(x_train, perc_feature, verbose=0)
-    x_train *= non_perc_vec
-    epoch += 1
-    if epoch >= epochs_perc:
-        break
+trainer = Trainer(percnet, network, optimizer, verbose)
+history_list = LossAccHistory()
+history_list = trainer.train(x_train, y_train,
+                             x_val, y_val,
+                             subdt_size,
+                             batch_size,
+                             epochs_prior, epochs_perc, epochs_adj,
+                             decay,
+                             history_list)
 
-# 微調整
-'''
-# 統合サブネットの重み固定を解除
-for i in range(3 * (layers_intnet - 1) + 2):
-    network.layers[-i-1].trainable = True
-network.compile(optimizer=optimizer, loss=categorical_crossentropy, metrics=['accuracy'])
-network.summary()
-'''
-if True:    # 微調整を行う条件を入れる(現状は常に微調整を行う)
-    non_perc_vec[:subdt_size] = 0
-    x_train *= non_perc_vec
-    network.fit(x_train, y_train,
-                initial_epoch=epochs_prior+epochs_perc,
-                epochs=epochs_prior+epoch+epochs_adj,
-                batch_size=batch_size,
-                verbose=verbose,
-                validation_data=(x_val, y_val),
-                callbacks=[history_list])
-
-
+# 損失と精度の評価
 score_train = network.evaluate(x_train, y_train, batch_size=batch_size)
 score_val = network.evaluate(x_val, y_val, batch_size=batch_size)
 score_test = network.evaluate(x_test, y_test, batch_size=batch_size)
@@ -203,6 +103,7 @@ print('Train - loss:', score_train[0], '- accuracy:', score_train[1])
 print('Validation - loss:', score_val[0], '- accuracy:', score_val[1])
 print('Test - loss:', score_test[0], '- accuracy:', score_test[1])
 
+# 損失と精度をグラフにプロット
 plt.figure()
 plt.plot(history_list.accuracy)
 plt.plot(history_list.accuracy_val)
